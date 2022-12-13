7828650e149d6933a9dcb0aeb0b2a745d6bf39b3
kishanstar12 <karan.saxena9425@gmail.com>
2022-12-13 23:54:45 +0000

    task4

---
+++ task4/dom.js
@@ -0,0 +1,21 @@
+//console.dir(document);
+
+//console.dir(document.domain);
+//console.dir(document.URL);
+//console.dir(document.title);
+//console.dir(document.head);
+//console.dir(document.all[10]);
+//console.dir(document.forms);
+
+//var headerTitle=document.getElementById('header-title');
+//var header=document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent='Have Fun';
+//headerTitle.innerText='dont run';
+
+//headerTitle.innerHTML='<h3>why so serious</h3>';
+//header.style.borderBottom='solid 3px #000';
+var textOfTitle='ADD ITEM';
+var titleFontChange=document.querySelector('.title');
+titleFontChange.innerHTML=textOfTitle.bold();
+titleFontChange.style.color='green';
