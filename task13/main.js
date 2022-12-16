18fee7ec103c9c4f81c44156b5cf91fe254f94c5
kishanstar12 <karan.saxena9425@gmail.com>
2022-12-16 12:06:07 +0000

    task 13 done

---
+++ task13/main.js
@@ -0,0 +1,50 @@
+var form = document.getElementById('my-form');
+
+//var userName = document.getElementById('name');
+//var email = document.getElementById('email');
+
+//console.log(userName);
+//console.log(email);
+
+form.addEventListener('submit', addLocalStorage);
+
+function addLocalStorage(e) {
+    e.preventDefault();
+    var user=document.getElementById('users');
+    user.textContent='';
+    var userName = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+
+    //var str=`{"name":"${userName}","email":"${email}"}`;
+
+    //console.log(str);
+
+    //var json=JSON.parse(str);
+    //console.log(json);
+
+    var obj={
+        name:userName,
+        email:email
+    }
+
+    localStorage.setItem(`usedDeatails${email}`,JSON.stringify(obj) );
+    userName = '';
+    email = '';
+
+    for (const key in localStorage) {
+        var obj1=JSON.parse(localStorage.getItem(key));
+        //console.log(obj1);
+        var newDiv=document.createElement('li');
+        var newDivText=document.createTextNode(`${obj1.name} ${obj1.email}`);
+        newDiv.appendChild(newDivText);
+        newDiv.style.textAlign='centre';
+        newDiv.style.fontSize='20px';
+        newDiv.style.listStyle.type='circle';
+        //var user=document.getElementById('users');
+        user.appendChild(newDiv);
+      }
+}
+
+
+
+//userDetailskishangautam945@gmail.com userDetailskishangautam945@gmail.comm userDetailskishangautam945@gmai.comm
